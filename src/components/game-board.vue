51a32c82cfc23b19b6791738028c620c690752ab
Sander Moolin <sandermoolin@gmail.com>
2017-09-05 10:15:07 -0400

    Board colors alternate correctly

---
+++ src/components/game-board.vue
@@ -0,0 +1,61 @@
+<template>
+
+  <div class="game-board">
+
+    <div v-for="row in height" class="row">
+
+      <div
+        v-for="col in width"
+        :x="col"
+        :y="row"
+        class="cell"
+        :index="getIndex(col, row)"
+        :style="{ 'background-color': colors[(getIndex(col, row) - row) % colors.length] }"
+        >
+
+
+
+      </div>
+
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  props: {
+    colors: {
+      default: ['#fff', '#000'],
+      type: Array
+    },
+    width: {
+      default: 8,
+      type: Number
+    },
+    height: {
+      default: 8,
+      type: Number
+    }
+  },
+  methods: {
+    getIndex (col, row) {
+      return (row - 1) * this.height + col
+    }
+  }
+}
+
+</script>
+
+<style>
+
+.cell {
+  display: inline-block;
+  width: 100px;
+  height: 100px;
+  border: 2px solid #000;
+}
+
+</style>
